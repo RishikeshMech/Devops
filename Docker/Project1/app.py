fb5724615f3aea847ab0446a6a3146fd11f43732
Ubuntu <ubuntu@ip-172-31-81-196.ec2.internal>
2024-10-01 14:16:20 +0000

    new repo

---
+++ Docker/Project1/app.py
@@ -0,0 +1,3 @@
+print("Hello Rishi if You are getting this line means you have successfully craeted image and is working")
+print("All The Very Best For Your Devops Jorney")
+print("I know youll succseed")
